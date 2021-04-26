d98faa74e0ecc36afc5709314c67e56b63eb0045
alphakys <gys1120@naver.com>
2021-04-26 18:19:48 +0900

    달팽이 문제 해결

--- src/algorithm/Boj2869.java
+++ src/algorithm/Boj2869.java
@@ -1,14 +1,17 @@
 package algorithm;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Boj2869 {
 
 	public static void main(String[] args)throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		String[] str = br.readLine().split(" ");
 		
@@ -16,25 +19,36 @@ public class Boj2869 {
 		int B = Integer.parseInt(str[1]);
 		int V = Integer.parseInt(str[2]);
 		
-		int day = 0;
-		int night = 0;
-		
 		if(A==V) {
-			System.out.println(1);
-		}else{
+			bw.write(String.valueOf(1));
+		}else if((A-B) != 1) {
 			
-			for(int i=1; ;i++) {
-				day += A*i;
+			
+			int answer = (V-A)/(A-B);
+			
+			if(answer>0) {
 				
-				if(day <V) {
-					night += (A-B);
-					day = night;
+				if((V-A)%(A-B)==0) {
+					answer = answer+1;
+					bw.write(String.valueOf(answer));
+				}else {
+					answer = answer+2;
+					bw.write(String.valueOf(answer));
 				}
+				
+			}else {
+				bw.write(String.valueOf(2));
 			}
 			
 			
+		}else {
+			bw.write(String.valueOf((V-A)+1));
 		}
-		
+			
+		bw.flush();
+		bw.close();
+		br.close();
+
 	}
 
 }

de1329c32c0db9cd98a13b80c5128777abd2e275
alphakys <gys1120@naver.com>
2021-05-11 22:24:07 +0900

    알고리즘

---
+++ src/algorithm/Boj1018.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1018 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = br.readLine().split(" ");
+		
+	}
+
+}

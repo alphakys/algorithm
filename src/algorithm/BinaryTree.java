1cb1ef8399814011b52fb71070cc66b43cc818cd
alphakys <gys1120@naver.com>
2021-06-08 12:13:43 +0900

    이진 탐색트리 중위 순회

--- src/algorithm/BinaryTree.java
+++ src/algorithm/BinaryTree.java
@@ -1,112 +1,6 @@
 package algorithm;
 
-class Node{
-		
-	int data;
-	Node left;
-	Node right;
-	
-	public Node(int value) {
-		this.data = value;
-		this.left = null;
-		this.right = null;
-	}
-		
-}
 
-public class BinaryTree{
-
-	Node rootNode;
-	
-	public void add(int value) {
-		
-		
-		if(rootNode==null) {
-				rootNode = new Node(value);
-				System.out.println("root:"+value);
-				return;
-		}else {
-			
-			Node parent = rootNode;
-			Node current;
-			
-			while(true) {
-				
-				current = parent;//루트
-				if(parent.data>value) {
-					
-					parent = parent.left;//레프트
-					
-					if(parent==null) {
-						current.left = new Node(value);
-						return;
-					}
-				}else {
-					
-					current = parent;//루트
-					if(parent.data>value) {
-						
-						parent = parent.left;//레프트
-						
-						if(parent==null) {
-							current.left = new Node(value);
-							return;
-						}
-					}
-			
-				}
-			
-			}
-			
-		}
-		
-	}
-	
-	public void remove(int value) {
-		
-		if(rootNode==null) {
-			System.out.println("노드가 존재하지 않습니다.");
-		}else {
-			
-			Node parent = rootNode;
-			Node current;
-			
-			while(true) {
-				
-			}
-		}
-		
-		
-		
-		
-	}
-	
-	
-	
-	
-public static void main(String[] args) {
-	
-	BinaryTree bt = new BinaryTree();
-	bt.add(10);
-	bt.add(7);
-	bt.add(6);
-	
-}
-	
-	
-	
-}
-
-
-
-
-
-
-
-
-
-
-/*
 public class BinaryTree {
 	//필드
 	Node rootNode = null;
@@ -117,19 +11,19 @@ public class BinaryTree {
 		Node left;
 		Node right;
 		
-		public Node(int data) {
+		public Node(int value) {
 			this.data = data;
-			left = null;
-			right = null;
+			this.left = null;
+			this.right = null;
 		}
 		
 	}
 	
 
-	public void add(int data) {
+	public void add(int value) {
 		
 		if(rootNode ==null) {
-			rootNode = new Node(data);
+			rootNode = new Node(value);
 			
 		}
 			
@@ -140,12 +34,12 @@ public class BinaryTree {
 			
 			current = parent;
 			
-			if(parent.data> data) {
+			if(parent.data> value) {
 				
 				parent = parent.left;
 				
 				if(parent ==null) {
-					current.left = new Node(data);
+					current.left = new Node(value);
 					break;
 				}
 		
@@ -154,7 +48,7 @@ public class BinaryTree {
 				parent = parent.right;
 				
 				if(parent ==null) {
-					current.right = new Node(data);
+					current.right = new Node(value);
 					break;
 				}
 				
@@ -169,5 +63,37 @@ public class BinaryTree {
 		
 	}	
 	
+	public void inOrder() {
+		
+		Node parent = rootNode;
+		Node current;
+		
+		while(true) {
+			current = parent;
+		
+			if(parent.left!=null) {
+				parent = parent.left;
+			}
+			
+		}
+		
+	}
+	
+	
+	//		10
+	//	7		11
+	//6	  8
+	
+public static void main(String[] args) {
+	
+	BinaryTree bt = new BinaryTree();
+	bt.add(10);
+	bt.add(7);
+	bt.add(6);
+	bt.add(8);
+	bt.add(11);
+	
 }
-*/
+
+}
+

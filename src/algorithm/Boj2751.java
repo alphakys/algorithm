f87cce53fb2a43ed5ba9d1b65650309521dbecd8
alphakys <gys1120@naver.com>
2021-06-09 16:32:02 +0900

    이진 탐색 트리 스택으로 구현 연습

--- src/algorithm/Boj2751.java
+++ src/algorithm/Boj2751.java
@@ -3,62 +3,114 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Stack;
 
 public class Boj2751 {
 	
+	static class BST{
+		
+		class Node{
+			
+			int data;
+			Node left;
+			Node right;
+			
+			public Node(int value) {
+				this.data = value;
+				this.left=null;
+				this.right=null;
+			}
+		}	
+		Node rootNode;	
+			
+		public void add(int value) {
+			
+			if(rootNode==null) {
+				rootNode = new Node(value);
+				return;
+			}else {
+				
+				Node parent = rootNode;
+				Node current;
+				
+				while(true) {
+					current = parent;
+				
+					if(parent.data>value) {
+						parent = parent.left;
+						
+						if(parent==null) {
+							current.left = new Node(value);
+							break;
+						}
+					}else {
+						parent = parent.right;
+						
+						if(parent==null) {
+							current.right = new Node(value);
+							break;
+						}
+					}
+					
+				}	
+			}	
+		}
+		//add function		
+		
+		//			13
+		//	  10  		14
+		//	 7	11
+		//	4 8 
+		
+		public void show(Node node) {
+			
+			Stack<Node> st = new Stack<>();
+			
+			while(true) {
+				
+				if(node!=null) {
+					st.push(node);
+					node = node.left;
+					if(node==null) {
+						System.out.println(st.pop().data);
+					}
+					
+				}
+		
+			}
+			
+			
+			
+		}
+
+		public void inOrder(Node node) {
+			
+			if(node !=null) {
+				if(node.left!=null) inOrder(node.left);
+				System.out.println(node.data);
+				if(node.right!=null) inOrder(node.right);
+			}else {
+				System.out.println("rootNode가 존재하지 않습니다.");
+			}
+		}//inOrder function
+			
+	}//BST class
+	
 	public static void main(String[] args)throws IOException {
 		
-		int[] tmp;
-		int[] arr = {38, 27, 43, 3, 9, 82, 10};
-		
-		int size = arr.length;
-		
-		int start = arr[0];
-		int end = arr[size];
-		
-		//if(start>)
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BST bst = new BST();
 		
 		int N = Integer.parseInt(br.readLine());
-		int max = (int)(N*1.4);
-		ArrayList<Integer> arr = new ArrayList<>(max);
-		
+	
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(br.readLine());
-			arr.add(num);
+			bst.add(num);
+			
 		}
-		Collections.sort(arr);
 		
-		int n = arr.size();
+		bst.show(bst.rootNode);
 		
-		for(int i=0;i<n;i++) {
-			System.out.println(arr.get(i));
-		}
-		*/
 	}
 
 }

c10562d694d3a1aecd3c21758214e11814c0aa05
alphakys <gys1120@naver.com>
2021-06-14 16:52:01 +0900

    합병 정렬

--- src/algorithm/Boj2751.java
+++ src/algorithm/Boj2751.java
@@ -13,7 +13,7 @@ public class Boj2751 {
 	}
 	
 	public static void main(String[] args)throws IOException {
-		
+		/*
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
@@ -24,13 +24,44 @@ public class Boj2751 {
 			arr[i] = num;
 		}
 		
+		*/
 		
+	int ar[] = {2,4,6,8};
+	int ar1[] = {10,5,7,9};
+	
+	int sort[] = new int[8];
+	
+	int size = ar.length;
+	
+	int f = 0;
+	int p = 0;
+	int i = 0;
+
+	while(f<3 || p<3) {
 		
+	
+			if(ar[f] > ar1[p]) {
+				System.out.println("p--"+p);
+				sort[i] = ar1[p];
+				System.out.println("p:"+sort[i]);
+				i++;p++; 
+			}
+			else{
+				System.out.println("f--"+f);
+				sort[i] = ar[f];
+				System.out.println("f:"+sort[i]);
+				i++;f++;
+			}
 		
+	
+	}
 		
-		
-		
-		
+	
+	
+	
+	
+	
+	
 	}
 
 }

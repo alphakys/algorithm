be8ce609d094687aca2ef86c3dedbcbc26070668
alphakys <gys1120@naver.com>
2021-06-18 17:48:46 +0900

    6.18 sorting 알고리즘 생각중

--- src/algorithm/MergeSort.java
+++ src/algorithm/MergeSort.java
@@ -1,55 +1,106 @@
 package algorithm;
 
 public class MergeSort {
-
+	
+	int[] arr;
+	int[] tmp;
+	
+	
+	public void sorting(int start, int end) {
+		
+		start = 1;
+		end = arr.length;
+		int mid = end/2;
+		
+		arr= new int[mid];
+		tmp = new int[end-mid];
+		
+		if(arr.length==1) {
+			break;
+		}else {
+			sorting(start, end);
+		}	
+			
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		//두 배열을 비교해서 정렬하는 코드
 		int ar[] = {2,4,6,8};
-		int ar1[] = {10,5,7,9};
+		int ar1[] = {3,5,7,9};
 		
 		int sort[] = new int[8];
 		
-		int size = ar.length;
+		int size = ar.length-1;
+		int size1 = ar1.length-1;
 		
 		int f = 0;
 		int p = 0;
 		int i = 0;
-
-		while(f<3 || p<3) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		
+		while(i<8) {
+			
+			//한 배열이 먼저 배열의 끝에 도달했다면 그 다음엔 어떻게 처리해줘야 하나?
+			if(f==size && p<size1) {
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			if(f<ar.length-1 && p<ar.length-1) {
 				if(ar[f] > ar1[p]) {
 					
-					System.out.println("p--"+p);
+					System.out.println("p:"+p);
 					sort[i] = ar1[p];
-					System.out.println("p:"+sort[i]);
+					System.out.println("value:"+sort[i]);
 					i++;p++; 
 				}
 				else{
-					System.out.println("f--"+f);
+					System.out.println("f:"+f);
 					sort[i] = ar[f];
-					System.out.println("f:"+sort[i]);
+					System.out.println("value:"+sort[i]);
 					i++;f++;
 				}
 			}else {
 				if(ar[f] > ar1[p]) {
 					
-					System.out.println("2p--"+p);
+					System.out.println("2p:"+p);
 					sort[i] = ar1[p];
-					System.out.println("p:"+sort[i]);
-					i++; 
+					System.out.println("value2:"+sort[i]);
+					sort[i+1] = ar[f];
+					break;
 				}
 				else{
-					System.out.println("2f--"+f);
+					System.out.println("2f:"+f);
 					sort[i] = ar[f];
-					System.out.println("f:"+sort[i]);
-					i++;
+					System.out.println("value2:"+sort[i]);
+					sort[i+1] = ar[p];
+					break;
 				}
 			}
 				
 			
-		
+		*/
 		}
 			
 		

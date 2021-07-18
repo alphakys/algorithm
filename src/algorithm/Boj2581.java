39e44f1f2b5f2d47005fdb79620c204f5580ab41
alphakys <gys1120@naver.com>
2021-07-18 17:43:59 +0900

    속도 줄임

--- src/algorithm/Boj2581.java
+++ src/algorithm/Boj2581.java
@@ -3,10 +3,8 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map.Entry;
+
 
 public class Boj2581 {
 
@@ -18,58 +16,77 @@ public class Boj2581 {
 		
 		boolean[] isPrime = new boolean[N-M+1];
 		Arrays.fill(isPrime, true);
-
+		int min=0;
 		for(int i=M; i<=N; i++) {
 			
 			if(i==1) {
 				isPrime[0]=false;
-			}else if(i==2) {
-				
+			}else if(i==2) {	 
+				min=2;
 				if((N/2)>=2) {
 					for(int a=2;a<=(N/2);a++) {
 						
-						isPrime[(2*a)-1]=false;
+						isPrime[(2*a)-M]=false;
 					}
 				}
 			}else {
-				
-				for(int j=2; j<i; j++) {
+			
+				if(isPrime[i-M]==true) {
+					
 					int count=0;
-					if(isPrime[i-1]==true) {
+					
+					for(int j=2; j<i; j++) {
 						
-						if(i%j!=0) {
-							count++;
-							if(count==(i-2))
+							if((i%j)!=0) {
+								count++;
 								
-								if((N/i)>=2) {
-									for(int a=2;a<=(N/i);a++) {
+								if(count==(i-2)) {
+									
+									min = i;
+									
+									if((N/i)>=2) {
 										
-										isPrime[(i*a)-1]=false;
+										for(int a=2;a<=(N/i);a++) {
+											
+											isPrime[(i*a)-M]=false;
+										}
 									}
-								}
+								}	
 								
+							}else {
+								isPrime[i-M]=false;
 							
+								break;
+							}
 						}
-					}
 					
 				}
 			
 			}	
 		}
 		int sum=0;
-		int arr = new int[1];
-		for(int z=0;z<isPrime.length;z++) {
+		int len = isPrime.length;
+		
+		for(int z=0;z<len;z++) {
 			
 			if(isPrime[z]==true) {
-				min = z;
-				sum +=z+1;
+				//System.out.println("소수:"+(z+M));
+				if(min>(z+M)) {
+					min = (z+M);
+				}
+				sum +=(z+M);
 			}
 		}
 		
-		if(sum==0)
+		
+		
+		if(sum==0) {
 			System.out.println(-1);
-		else
+		}else {
 			System.out.println(sum);
+			System.out.println(min);
+		}
+			
 	}	
 		
 }

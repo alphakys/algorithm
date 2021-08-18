3a6c5c698e1dbc448ef584c020aaad81ac9c5df5
alphakys <gys1120@naver.com>
2021-08-18 22:55:46 +0900

    f

---
+++ src/algorithm/Boj1003.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1003 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			
+		}
+	}
+
+}
--- src/algorithm/Boj10870.java
+++ src/algorithm/Boj10870.java
@@ -1,9 +1,37 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Boj10870 {
-
-	public static void main(String[] args) {
-
+	
+	public static int fibonacci(int N) {
+		
+		if(N==0) {
+			System.out.println(0);
+			return 0;
+		}else if(N==1) {
+			System.out.println(1);
+			return 1;
+		}else {
+			return fibonacci(N-1) + fibonacci(N-2);
+		}
+		
+	}
+	
+	
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[41][41];
+		
+		//System.out.println(fibonacci(N));
+		
+		
+		
+		
 	}
 
 }

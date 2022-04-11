b64e7a1b054b443a85b7ba2fc9cca22215f69793
강용수 <74286771+alphakys@users.noreply.github.com>
2022-04-11 18:27:43 +0900

    2022.04.11 mergeSort

--- c/mergeSort.c
+++ c/mergeSort.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void change(int* arr, int left, int right){
-
-    int change = arr[left];
-    arr[left] = arr[right];
-    arr[right] = change;
+void printArray(int *arr, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
-void divide(int* arr, int left, int right){
+void divide(int* arr, int left, int right, int* sorted){
     
     int mid = (right - left + 1) / 2;
     
@@ -17,30 +18,124 @@ void divide(int* arr, int left, int right){
         mid = left + mid;
         //printf("left : %d, right : %d mid : %d\n", left, right, mid);
         //return 0;
-        divide(arr, left, mid - 1); // 0,3
-        divide(arr, mid, right);   // 4,7 --> mid=2 ==> (4,1)(2,7)
-    }
-
-    if(arr[left] >arr[right]){
+        divide(arr, left, mid - 1, sorted); // 0,3 (0,1) (2,3)
+        divide(arr, mid, right, sorted);   // 4,7 --> mid=2 ==> (4,1)(2,7)
         
-        change(arr, left, right);
-
-        for (int i = 0; i < 8; i++){
-            //printf("%d ", arr[i]);
-        }
     }
+mergeArr(arr, sorted, left, right, mid);
+    int pointer = left;
 
+    int leftPos = left; 
+    int rightPos = right; 
+    int midPos = left + ((right - left + 1) / 2);
+
+printf("left : %d, midPos : %d, right : %d, mid : %d\n", leftPos, midPos, rightPos, mid);
+
+    for(int i=0; i<right-left+1; i++)
+    {
+        
+        if(left>=midPos){
+            sorted[pointer++] = arr[rightPos++];
+        }else if(midPos>rightPos){
+            sorted[pointer++] = arr[leftPos++];
+        }else{
+  
+            if(arr[leftPos] > arr[midPos]){
+                sorted[pointer++] = arr[midPos++];
+                
+            }else{
+                sorted[pointer++] = arr[leftPos++];
+            
+            }
+
+        }
+        
+        printf("sorted : ");printArray(sorted, 8);
+    }
+    
+    for(int i=left; i<=right; i++){
+        arr[i] = sorted[i];
+    }
+    printf("arr : ");printArray(arr, 8);
+ 
 }
 
-//       (0,7)
-//  (0,3)        (4,7)
-//(0,1)(2,3)   (4,5)(6,7)
+void mergeArr(int* arr, int* sorted, int left, int right, int mid){
+
+    int pointer = 0;
+   // int right = 0;
+   // int left = 0;
+
+    for(int i=0; i<=right; i++)
+    {
+        if(left>mid-1){
+            
+            sorted[pointer++] = arr[right++];
+        }else if(right>mid){
+            sorted[pointer++] = arr[left++];
+        }else{
+  
+            if(arr[left] > arr[right]){
+                sorted[pointer++] = arr[right++];
+                
+            }else{
+                sorted[pointer++] = arr[left++];
+            
+            }
+
+        }
+
+        //printf("left : %d right : %d\n", left, j);
+    }
+}
 
 
 
 int main(){
 
-    int N = 8;
+    //int N = 8;
+    //int* sorted = malloc(sizeof(int)*(N));
+/*
+    int arr1[4] = {10, 12, 20, 21};
+    int arr2[4] = {13, 15, 22, 25};    
+
+    
+
+    int pointer = 0;
+    int right = 0;
+    int left = 0;
+
+    for(int i=0; i<8; i++)
+    {
+        if(left>3){
+            printf("left : %d, right : %d, arr : %d\n", left, right, arr2[right]);
+            sorted[pointer++] = arr2[right++];
+        }else if(right>3){
+            sorted[pointer++] = arr1[left++];
+        }else{
+  
+            if(arr1[left] > arr2[right]){
+                sorted[pointer++] = arr2[right++];
+                
+            }else{
+                sorted[pointer++] = arr1[left++];
+            
+            }
+
+        }
+
+        //printf("left : %d right : %d\n", left, j);
+    }
+    
+
+
+    for (int i = 0; i < N; i++){
+        printf("%d\n", sorted[i]);
+    }
+    free(sorted);
+    return 0;
+*/
+
 
     /*scanf("%d", &N);
 
@@ -50,16 +145,17 @@ int main(){
         scanf("%d", &arr[i]);
     }
 */
-    int arr[8] = {27, 10, 12, 20, 25, 13, 15, 22};
+    int arr[8] = {21, 10, 12, 20, 25, 13, 15, 22};
 
-    //int sorted[];
+    int* sorted = malloc(sizeof(int)*(8));
 
-    divide(arr, 0, 7);
+    divide(arr, 0, 7, sorted);
     
-    for (int i = 0; i < N; i++){
-        printf("%d\n", arr[i]);
+    for (int i = 0; i < 8; i++){
+        printf("%d\n", sorted[i]);
     }
 
+    free(sorted);
 /*
     for (int insert = 1; insert < N; insert++)
     {

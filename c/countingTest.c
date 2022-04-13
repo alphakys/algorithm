ad11d40748673489492b700b8cd2d99513e08c6c
alphakys <gys1120@naver.com>
2022-04-13 23:13:10 +0900

    2022.04.13

--- c/countingTest.c
+++ c/countingTest.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 10001
+#define MAX_SIZE 10000
 
 void change(int* arr, int* i, int* insert){
 
@@ -12,38 +12,44 @@ void change(int* arr, int* i, int* insert){
 
 
 int main(){
-
+/*
     int N;
     scanf("%d", &N);
    
-    short* arr = malloc(sizeof(short)*N);
+    int* arr = malloc(sizeof(int)*N);
 
     for (int i = 0; i < N; i++){
         scanf("%d", &arr[i]);
     }
+*/
 
-    short* sort = malloc(sizeof(short)*N);
-    short* count= malloc(sizeof(short)*N);
+
+
+int N = 13;
+int arr[13] = {5,5,3,4,5,1,4,1,3,2,4,2,3};
+
+    int sort[MAX_SIZE] = {0};
+    //int count[MAX_SIZE];
 
     for (int i = 0; i < N; i++){
         
         //sort[i] = 0;
-        count[i] = 1; 
+        //count[i] = 1; 
     }
 
 
-    short limit = 0;
+    int limit = 0;
 
     for (int i = 0; i < N; i++)
     {
-        
+        //i=2 j=0 j=1
         for (int j = 0; j < N; j++)
         {
 
             if (arr[i] == sort[j])
             {
-                count[j]++;
-                sort[j] = arr[i];
+                //count[j]++;
+                sort[j] += arr[i];
                 break;
             }
             else if(arr[i] != sort[j] && sort[j]<1)
@@ -58,7 +64,7 @@ int main(){
                         if(sort[k]> sort[insert]){
                             
                             change(sort, &k, &insert);
-                            change(count, &k, &insert);
+                            //change(count, &k, &insert);
                             insert--;
                             
                         }
@@ -79,17 +85,16 @@ int main(){
 
     while (sort[z]>0)
     {
-        
+        /*
         for (int i = 0; i < count[z]; i++)
         {
             printf("%d\n", sort[z]);
         }
-        
+        */
+       printf("%d\n  count %d", sort[z], sort[z]);
         z++;
     }
     
-    free(arr);
-    free(sort);
-    free(count);
+    //free(arr);
     return 0;
 }

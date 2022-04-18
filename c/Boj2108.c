04bca7b65d11740b0d52dec33558f2c1f5302aa1
alphakys <gys1120@naver.com>
2022-04-18 23:26:19 +0900

    22.4.18

--- c/Boj2108.c
+++ c/Boj2108.c
@@ -53,14 +53,13 @@ int main(){
     //memset(maxArr, -1, sizeof(int)*MAX_SIZE);
     int maxCount = 0;
 
-    for (int i = -10; i < 10; i++)
+    for (int i = -4000; i < 4001; i++)
     {   
         if(i<0){
             
             if(negative[abs(i)]>0){
                 
                 arr[count++] = i;
-               printf("up"); printArray(arr,10);
                 
                 if(maxCount < negative[abs(i)]){
                     
@@ -77,9 +76,9 @@ int main(){
             }   
         }else{
             if(positive[i]>0){
-                printf("arr  %d  ", i);
+
                 arr[count++] = i;
-                printf("down"); printArray(arr,10);
+
                 if(maxCount < positive[i]){
                     
                     for(int k=0; k<8002; k++){
@@ -96,25 +95,30 @@ int main(){
 
             }
         }
-printf("ahdi");printArray(arr,10);printf("i : %d\n", i);
+
     }
 
+    double divide = (double)sum / (double)N;
 
-    printf("--------------------\n");
-    
-    printf("%d\n", (int)(round((double)round(sum/N))));
-    
+    //printf("dd %f\n", round(divide));
+    double a = 1.1;
+    double b = 5.6;
+
+    printf("%f", round(a+b));
+
+    printf("%d\n", (int)(round((double)round(sum / N))));
+
     printf("%d\n", arr[(count / 2)]);
 
-     for(int i=0; i<20; i++){
+     for(int i=0; i<8002; i++){
 
         if(maxArr[i]==4001){
 
             if(i<2){
-                printf("max : %d\n", maxArr[i-1], i);
+                printf("%d\n", maxArr[i-1]);
                     break;
             }else{
-                printf("max : %d\n",maxArr[1], i);
+                printf("%d\n",maxArr[1]);
                     break;
             }
 
@@ -125,8 +129,8 @@ printf("ahdi");printArray(arr,10);printf("i : %d\n", i);
     printf("%d\n", arr[count-1] - arr[0]);
     
     
-    printArray(arr,10);
-    printArray(positive,10);
+    //printArray(arr,10);
+    //printArray(maxArr,10);
     
     return 0;
 

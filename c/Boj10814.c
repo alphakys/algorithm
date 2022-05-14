077e6c98f08181e0c98f6d3c3f1bc0d11bb347a4
alphakys <gys1120@naver.com>
2022-05-14 12:23:32 +0900

    22.5.14

--- c/Boj10814.c
+++ c/Boj10814.c
@@ -3,47 +3,18 @@
 #include <string.h>
 #include <math.h>
 
-int strToint(int start, char* str){
-
-    int digit=0;
-    char* digit_flag = str;
-
-    while(!(*digit_flag ==32)){
-
-        digit_flag++;
-        digit++;
- 
-    }
-
-    int sum=0;
-
-    int poww = (digit-start)-1;
-
-    for (int j = start; j < digit; j++)
-    {
-    
-        double n = (double)str[j] - (double)48;
-        double powNum = pow((double)10,(double)(poww));
-
-        sum += (int)(n*powNum);
-        poww--;
-    
-    }
-    
-    return sum;
-}
-
 typedef struct string{
-    char s[105];
+    char name[101];
+    int age;
     int index;
 } string;
 
-int compare(const string *a, const string *b){
-    int n1 = strToint(0, (char *)(a->s));
-    
-    int n2 = strToint(0, (char *)(b->s));
+int compare(const void *a, const void *b){
+    string *str1 = (string *)a;
+    string *str2 = (string *)b;
+    int n1 = str1->age;
 
-    //printf("return %d : %d    %d : %d \n", n1, n2, a->index, b-> index);
+    int n2 = str2->age;
 
     if(n1 > n2){
         return 1;
@@ -51,7 +22,7 @@ int compare(const string *a, const string *b){
         return -1;
     }else{
         
-        if( a->index >  b->index){
+        if( str1->index >  str2->index){
             //printf("return %d : \n", 1);
             return 1;
         }else{
@@ -62,6 +33,7 @@ int compare(const string *a, const string *b){
     
 }
 
+string list[100000];
 
 int main(){
 
@@ -69,71 +41,18 @@ int main(){
     scanf("%d", &N);
     getchar();
 
-    string* list = (string *)malloc(sizeof(string)*100001);
-/*
-    for(int i=1; i<100001; i++){
-        list[i] = (string *)malloc(sizeof(string));
-        memset(list[i],0,sizeof(string));
-    }
-*/
-    string str;
+    for(int i=0; i<N; i++){
+        scanf("%d %s", &(list[i].age), &(list[i].name));
 
-    for(int i=1; i<=N; i++){
-        //scanf("%s", str);
-        fgets(str.s, 105, stdin);
-
-        char* ch = str.s;
-
-        int k=0;
-
-        while((*ch)){
-            if((*ch)==10 || (*ch)==30){
-                break;
-            }
-            
-            list[i].s[k] = *ch;
-            k++;
-            ch++;
-        }
         list[i].index = i;
         
     }
 
-/*
-    for(int i=1; i<=N; i++){
-        int len = strlen(list[i].s);
-        printf("index : %d\n", list[i].index);
-        for(int j=0; j <len; j++){
-            
-            printf("%c", list[i].s[j]);
-        }
-        printf("\n");
+    qsort(&list[0], N, sizeof(list[0]), compare);
+
+    for(int i=0; i<N; i++){
+        printf("%d %s\n", list[i].age, list[i].name);
     }
-    */
-
-
-   /*
-    for(int i=1; i<=N; i++){
-        printf("%d %d", &list[i].s[i], list[i].index);
-        
-        printf("\n");
-    }
-*/
-
-    qsort(&list[1], N, sizeof(list[1]), compare);
-
-    for(int i=1; i<=N; i++){
-        int len = strlen(list[i].s);
-        
-        for(int j=0; j <len; j++){
-            
-            printf("%c", list[i].s[j]);
-        }
-        printf("\n");
-    }
-
-
-    free(list);
 
     return 0;
 }
--- c/test.c
+++ c/test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
 typedef struct list
 {
     int i;
@@ -31,60 +32,16 @@ int compare(const list *a, const list *b){
     }
     
 }
+*/
 
-
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/input.h>
+//#include <sstream>
+#include <unistd.h>
 
 
 void main(){
 
-    int a;
-    char name[4];
-    
-    scanf("%d %s", &a, &name);
-
-    printf("%d %s", a, name);
-/*
-    list l[30000];
-    
-    for(int i=0; i<30000; i++){
-                
-        l[i].index = i;
-        l[i].i = 1;
-        //printf("%d\n", l[i].i);
-                
-    }
-
-    qsort(l, 30000, sizeof(l[0]), compare);
-
-    for(int i=0; i<30000; i++){
-                
-        printf("%d\n", l[i].index);
-                
-    }
-*/    
-    
-
-/*
-string arr[5][5];
-
-
-
-
-        for(int j=0; j<5; j++){
-
-            memset(arr[j],NULL,sizeof(string)*5);
-
-            for(int i=0; i<5; i++){
-                printf("j : %d, i : %d, p : %d\n",j, i, arr[j][i]);
-            }
-            /*
-            if(!arr[1][j].character){
-                printf("%d %d", j,arr[1][j]);
-                break;
-            }
-        
-        }
-
-*/
-
+   
 }

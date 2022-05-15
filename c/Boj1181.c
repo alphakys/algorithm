e777e022971d2491480009f3a153a37bc0a290d4
alphakys <gys1120@naver.com>
2022-05-15 22:26:46 +0900

    22.5.15

--- c/Boj1181.c
+++ c/Boj1181.c
@@ -2,160 +2,107 @@
 #include <stdio.h>
 #include <string.h>
 
-int len(const char* ch){
-
-    //int c = (int)ch;
-    int length = 0;
-    
-    while(*ch){
-        
-        ch++;
-        length++;
-    }
-    
-    return length;
-
-}
-
 int strcompare(char* str1, char* str2){
 
     int i = 0;
     int flag = 0;
 
-    if(strlen(str1) > strlen(str2)){
-        return --flag;
-    }else if(strlen(str1) < strlen(str2)){
-        return ++flag;
-    }else{
-        
-        while (str1[i] || str2[i])
+    while (str1[i] || str2[i])
+    {
+        if (str1[i] > str2[i])
         {
-            if (str1[i] > str2[i])
-            {
-                flag--;
-                break;
-            }else if(str1[i] == str2[i]){
-                i++;
-                
-            }
-            else
-            {
-                flag++;
-                break;
-            }
+            flag--;
+            break;
+        }else if(str1[i] == str2[i]){
+            i++;
+            
         }
-        if(flag==0){
-            str2[0] = '0';
+        else
+        {
+            flag++;
+            break;
         }
-        return flag;
     }
+
+    if(flag==0){
+        str2[0] = '0';
+    }
+    return flag;
+    
 
     
 }
 
-void mergeSort(char** arr, int left, int right, char** sorted){
+typedef struct string {
+    char s[51];
+    int length;
+} string;
 
-    int mid = (right - left + 1) / 2;
 
-    if (mid >= 1)
-    {
-        mid = left + mid;
-       
-        mergeSort(arr, left, mid - 1, sorted);
-        mergeSort(arr, mid, right, sorted);  
-        
-    }
-        
-    int pointer = left;
+int compare(const void *a, const void *b){
+    string *str1 = (string *)a;
+    string *str2 = (string *)b;
+    int n1 = str1->length;
 
-    int leftPos = left; 
-    int rightPos = right; 
-    int midPos = left + ((right - left + 1) / 2);
+    int n2 = str2->length;
 
-    int m = left + ((right - left + 1) / 2);
+    if(n1 > n2){
+        return 1;
+    }else if(n1 < n2){
+        return -1;
+    }else{
 
-    for(int i=0; i<right-left+1; i++)
-    {
-        if(leftPos>=m){
-            sorted[pointer++] = arr[midPos++];
-            
-        }else if(midPos>rightPos){
-            sorted[pointer++] = arr[leftPos++];
-            
-        }else{
-        
-            int flag = strcompare(arr[leftPos], arr[midPos]);
-        
-            if (flag > 0)
-            {   
-                sorted[pointer++] = arr[leftPos++];
-            }
-            else
-            {
-                sorted[pointer++] = arr[midPos++];
-            }
+        int flag = strcompare(str1->s, str2->s);
+        if (flag <0)
+        {
+            //printf("return %d : \n", 1);
+            return 1;
         }
-        
-    }
-    
-    for(int i=left; i<=right; i++){
-        arr[i] = sorted[i];
-
+        else
+        {
+            //printf("return %d : \n", -1);
+            return -1;
+        }
     }
     
 }
 
-int main(){
+int main()
+{
 
     int N;
     scanf("%d", &N);
 
-    char str[51];
-
-    char arr[20000][51];
-
-    char** dest = (char **)malloc(sizeof(char*)*N);
+    string arr[20000];
 
     for (int i = 0; i < N; i++)
     {
 
-        scanf("%s", str);
+        scanf("%s", &(arr[i].s));
 
-        int j = 0;
-        char *ptr = str;
-
-        while (*ptr)
-        {   
-            arr[i][j++] = *(ptr++);
-        }
-        dest[i] = &arr[i][0];
-
+        int len = strlen(arr[i].s);
+        arr[i].length = len;
+        //printf("%c %d", arr[i].s[0], arr[i].length);
     }
-    
-    char** sorted = (char **)malloc(sizeof(char*)*N);
 
-    mergeSort(dest, 0, N-1, sorted);
-
-    //printf("----------------------------\n");
+    qsort(arr,N,sizeof(arr[0]), compare);
 
     for (int i = 0; i < N; i++)
     {
-        char *p = dest[i];
+        char *p = (arr[i].s);
 
         if(*p =='0'){
             continue;
         }
+
         while(*p){
-            
+
             printf("%c", *p);
             p++;
         }
         printf("\n");
-        
-    }
 
-    free(dest);
-    free(sorted);
-  
+    }
+   
     return 0;
 }

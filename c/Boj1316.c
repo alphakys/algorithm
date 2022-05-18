fa16f4294d44d76f2f4f6b26cf4c9bf691df7b76
alphakys <gys1120@naver.com>
2022-05-19 08:40:30 +0900

    22.5.19

--- c/Boj1316.c
+++ c/Boj1316.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void intArray(int *arr, int n) {
     int i;
@@ -43,18 +44,19 @@ int main(){
     char str[101];
 
     int minus = 0;
-// printf("%d", strcmp('a', 'a'));
-    
+
+    char *check = malloc(N);
+    int checkNum = 0;
     for (int i = 0; i < N; i++)
     {
-        two:scanf("%s", str);
+        scanf("%s", str);
         
         int len = strlen(str);
 
-         for(int j=0; j<len; j++){
-            //printf("j : %d \n", j);
+        for(int j=0; j<len; j++){
+            //printf("out\n");
             for(int k=j+1; k<len; k++){
-                
+                //printf("j : %d k : %d  jval : %c  kval : %c\n", j,k, str[j], str[k]);
                 if(str[j] - str[k] !=0){
                     
                     int flag = len - 1;
@@ -62,20 +64,21 @@ int main(){
                     {
                         if(str[j] == str[flag--]){
                             //printf("true\n");
-                            minus++;
+                            //minus++;
+                            check[i]++;
                             break;
                         }
                     }
                     j = k;
-                    goto two;
+                    
                 }
             }
             
         }
     }
     
-    //charArray(checked, 5);
-    printf("%d", N-minus);
+    charArray(check, 5);
+    //printf("%d", N-minus);
 
     return 0;
 }

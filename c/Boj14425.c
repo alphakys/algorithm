c4694d5e275270c466d5167336ec234cddfe8e76
alphakys <gys1120@naver.com>
2022-06-06 15:30:19 +0900

    22.6.6

--- c/Boj14425.c
+++ c/Boj14425.c
@@ -16,108 +16,23 @@ void charArray(char *arr, int n) {
     int i;
     for (i = 0; i < n; i++) {
 
-        printf("%d ", arr[i]);
+        printf("%c ", arr[i]);
     }
     printf("\n");
 }
 
+int compare(const void *a, const void *b){
 
-void strToint(char* str, int* arr, int* arr1, int method){
+    //printf("str : %s %s ", a, b);
+    char* s1 = (char *)a;
+    char* s2 = (char *)b;
 
-    int index = 0;
-    int start = 0;
-    int i = 0;
-    int sum = 0;
-    while (str[index] !=0)
-    {
-
-        if (str[index] == 32 || str[index] == 10)
-        {
-            int digit = index-start;
-            int poww = digit- 1;
-            
-            if ((int)str[start] == 45)
-            {
-                poww = poww - 1;
-
-                for (int j = start + 1; j < start+digit; j++)
-                {
-
-                    double n = (double)str[j] - (double)48;
-                    double powNum = pow((double)10, (double)(poww));
-
-                    sum += (int)(n * powNum);
-                    poww--;
-                }
-                sum = sum * -1;
-                   
-                }
-                else
-                {
-
-                    for (int j = start; j < start+digit; j++)
-                    {
-                    
-                        double n = (double)str[j] - (double)48;
-                        double powNum = pow((double)10,(double)(poww));
-
-                        sum += (int)(n*powNum);
-                        poww--;
-                    
-                    }
-
-                }
-
-                if(method==0){
-                    if (sum >= 0)
-                    {
-                        arr[sum]++;
-                    }
-                    else
-                    {
-                        arr1[sum*-1]++;
-                    }
-                }else{
-
-                    if (sum >= 0)
-                    {
-                        if(arr[sum]==1){
-                            printf("1");
-                        }
-                        else
-                        {
-                            printf("0");
-                        }
-                    }
-                    else
-                    {
-                        if(arr1[sum*-1]==1){
-                            printf("1");
-                        }
-                        else
-                        {
-                            printf("0");
-                        }
-                    }
-                    printf(" ");
-                }
-
-                sum = 0;
-
-                start = index + 1;
-                index++;
-                
-        }
-        else
-        {
-            index++;
-        }
-
-    }
+    if(strcmp(s1, s2)>0)
+        return 1;
+    else if (strcmp(s1, s2) < 0) 
+        return -1;
+    
 }
-
-int positive[10000001];
-int negative[10000001];
 
 int main()
 {
@@ -125,51 +40,22 @@ int main()
     int N,M;
     
     scanf("%d %d", &N, &M);
-    //getchar();
 
-    char s[501];
+    char strN[10000][501];
 
     for (int i = 0; i < N; i++)
-    {
-        scanf("%s", s);
+    {   
+        scanf("%s", strN[i]);
     }
 
-
-
-
-
-
-    while ((s = getchar()) != '\n')
-    {
-        str[str_i++] = s;
+    qsort(strN, N, sizeof(strN[0]), compare);
         
-    }
-    
-    str[str_i] = '\n';
-              
-    strToint(str, positive, negative, 0);
-
-    int M;
-
-    scanf("%d", &M);
-    getchar();
-
-    char s1;
-    char str1[M*9+(M+1)];
-    str_i = 0;
-    
-    while ((s1 = getchar()) != '\n')
+    for (int i = 0; i < N; i++)
     {
-        str1[str_i++] = s1;
-        
+        printf("%s\n", strN[i]);
     }
-    
-    str1[str_i] = '\n';
-              
-    strToint(str1, positive, negative, 1);
-
-    //printArray(positive, 10);
-    //printArray(negative, 10);
+    // charArray(strN, 5);
+    // printf("%d", strcmp(s, z));
 
     return 0;
 }
--- c/test.c
+++ c/test.c
@@ -23,8 +23,36 @@ int strToint(char* str){
 
 void main(){
 
-    char c[6] = "1123";
-    char a = 127;
-    //printf("%d", a);
-    strToint(c);
+    int N = 11;
+    int start = 0;
+    int end = N-1;
+
+    int search = 11001;
+
+    int arr[11] = {1, 4, 87, 111, 264, 324, 478, 494, 2300, 11000, 11001};
+                 //0  1   2   3    4    5    6    7     8     9       10
+    int pivot = N / 2;
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("root pivot : %d\n", arr[pivot]);
+        
+        if(arr[pivot]==search){
+            printf("find : %d i : %d\n", arr[pivot], i);
+            break;
+        }
+        else if(arr[pivot] >search)
+        {
+            end = pivot;
+            pivot = (start + pivot) / 2;
+        }else
+        {
+            start = pivot;
+            pivot = start+ (end-start+1)/2;
+        }
+        printf("start : %d , pivot : %d , end : %d\n", start, pivot, end);
+
+    }
+
+
 }

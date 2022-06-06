5c1745eb7408644cd96f9740185f4ffc566be4e8
alphakys <gys1120@naver.com>
2022-06-06 16:08:52 +0900

    binary search

--- c/test.c
+++ c/test.c
@@ -27,28 +27,26 @@ void main(){
     int start = 0;
     int end = N-1;
 
-    int search = 11001;
+    int search = 264;
 
     int arr[11] = {1, 4, 87, 111, 264, 324, 478, 494, 2300, 11000, 11001};
                  //0  1   2   3    4    5    6    7     8     9       10
-    int pivot = N / 2;
-
-    for (int i = 0; i < N; i++)
+    
+    while(start<=end)
     {
+        int pivot = (start+end) / 2;
         printf("root pivot : %d\n", arr[pivot]);
         
         if(arr[pivot]==search){
-            printf("find : %d i : %d\n", arr[pivot], i);
+            printf("find : %d\n", arr[pivot]);
             break;
         }
         else if(arr[pivot] >search)
         {
-            end = pivot;
-            pivot = (start + pivot) / 2;
+            end = pivot-1;
         }else
         {
-            start = pivot;
-            pivot = start+ (end-start+1)/2;
+            start = pivot+1;
         }
         printf("start : %d , pivot : %d , end : %d\n", start, pivot, end);
 

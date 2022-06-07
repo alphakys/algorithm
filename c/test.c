44e40716184cbb2c6de91e9c962114528e1dd471
alphakys <gys1120@naver.com>
2022-06-08 08:46:41 +0900

    22.6.8

--- c/test.c
+++ c/test.c
@@ -15,72 +15,64 @@ int compare(const void *a, const void *b){
     };
 }
 
+void printArray(int *arr, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 void strToint(char* str, int* arr, char delim){
-
-    int start = 0;
-    int end = 0;
+    
+    int arr_ptr = 0;
     int index = 0;
+    int s_flag = 0;
+    char s[10];
+
     while (str[index] != '\0')
     {
         if ( str[index] == delim )
         {
-            end = index - 1;
-
-            if ((int)str[start] == 45)
-            {
-            }
-
-        else
-        {
+            arr[arr_ptr++] = atoi(s);
+            s_flag = 0;
+            
+            memset(s,0,10);
             index++;
         }
-
+        else
+        {   
+            printf("%c\n", str[index]);
+            s[s_flag++] = str[index++];
+            
         }
+
+       
     }
+    arr[arr_ptr++] = atoi(s);
 }
 
 void main(){
+    char s[100] = "11 23 45 4 8 9 75";
+    int arr[10];
 
-    char s[100] = "Acbok Ceedrill Buslill";
+    //char delim = '';
+    strToint(s, arr, ' ');
+    printArray(arr, 10);
+
     char s1[11] = " Ceedrilb";
     char s2[11] = "Buslill";
 
-    int i = 0;
-    int len = strlen(s)-1;
-    //printf("%d", len);
-    //s[22] = 97;
-    //printf("%s\n", s);
+    //int i = 0;
+    //int len = strlen(s)-1;
 
-    char* s3;
+/*
     while (s1[i] != 0)
     {
-        //printf("%d", s1[i]);
-        //s[len++] = s1[i++];
         strcpy(s3,&s1[i++]);
-        // printf("%c\n", s[len]);
-
-        //printf("%s\n", s);
-        //i++;
     }
+*/
+    //printf("%s\n", s3);
 
-    printf("%s\n", s3);
-
-    // printf("cat : %s\n", strcat(s, s1));
-    // printf("%s", s);
-
-    /* printf("%s", strtok(s, " "));
-    char *ptr = strtok(s, " ");
-
-    while(ptr !=NULL){
-        printf("%s", ptr);
-        ptr = strtok(NULL, " ");
-
-    }
-    for (int i = 0; i < 3; i++)
-    {
-    //    printf("%s\n", arr[i]);
-    }
-
-    */
 }

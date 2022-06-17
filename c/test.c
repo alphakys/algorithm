eab02a678f852b92e25f319958fe218cecc17adf
강용수 <74286771+alphakys@users.noreply.github.com>
2022-06-17 18:36:48 +0900

    22.6.17

--- c/test.c
+++ c/test.c
@@ -4,18 +4,40 @@
 #include <math.h>
 #include <unistd.h>
 
-int buff_size = 10000;
+int buff_size = 1<<10;
 
 void main()
 {
     //printf("%c", 11&&2);
     char buff[buff_size], *p = buff;
+    
     //printf("%p %p", p, buff);
-    //
+    /*
+    if(p == buff+buff_size){
+        read(0, p=buff, buff_size);
+    }
+*/
+
+//unsigned int a = 1<<31;
+//printf("%u", a);
+read(0, buff, buff_size);
+    
+    /*
+    for(int i=0; i<3;i++){
+        
+        printf("%d", atoi(buff));
+        //getchar();
+        
+    }
+    */
     //fread(buff, sizeof(char), buff_size, stdin);
-    read(0, buff, buff_size);
-    int i = 0;
-    printf("%s", buff);
+    
+    //printf("%s", buff);
+
+
+
+    //int i = 0;
+    
     /*
     int i = 0;
     while (i<2)

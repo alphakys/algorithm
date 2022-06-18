7d8db4543c2b178ec0a1b5545e8fa815e3936b09
alphakys <gys1120@naver.com>
2022-06-18 13:19:12 +0900

    22.6.18

--- c/test.c
+++ c/test.c
@@ -4,29 +4,75 @@
 #include <math.h>
 #include <unistd.h>
 
-int buff_size = 1<<10;
+#define buff_size 5
+
+/*
+char buff[buff_size];
+char *p = buff;
+
+char* readChar(char ch){
+
+    if(p == buff+buff_size){
+        read(0, p=buff, buff_size);
+        return *p++;
+    }
+}
+*/
+
+void bit(){
+
+    int sum = 0;
+
+    char bit[9] = "00001111";
+    int len = 7;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int n = (bit[i]-48) << len--;
+        printf("%d\n", n);
+        sum += n;
+    }
+
+    printf("%d", sum);
+}
+
+//unsigned int a = 1 << 12; 11개 숫자 들어가면 됨
 
 void main()
 {
-    //printf("%c", 11&&2);
-    char buff[buff_size], *p = buff;
+    //1200000;
+
+    int sum = 15;
+
+    char bit[9] = "00001111";
+    int len = 7;
+
+    for (int i = 0; i < 8; i++)
+    {
+        int n = (bit[i]-48) << len--;
+        printf("%d\n", n);
+        sum += n;
+    }
+
     
-    //printf("%p %p", p, buff);
+    
+    
+    printf("%d", sum);
+    // printf("%d", print(b));
+    //  printf("%p %p", p, buff);
     /*
     if(p == buff+buff_size){
         read(0, p=buff, buff_size);
     }
 */
-
-//unsigned int a = 1<<31;
-//printf("%u", a);
-read(0, buff, buff_size);
+    // char c = ReadChar(); c & 16; ret = 10 * ret + (c & 15), c = ReadChar()
+    // unsigned int a = 1<<31;
+    // printf("%u", a);
     
     /*
-    for(int i=0; i<3;i++){
-        
-        printf("%d", atoi(buff));
-        //getchar();
+    for(int i=0; i<3; i++){
+        read(0, buff, buff_size);
+        //printf("%p %p %p\n", buff, p, buff+buff_size);
         
     }
     */

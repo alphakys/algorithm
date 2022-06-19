4b35c87524094e773f87333a87c6a0cb4a2551fb
alphakys <gys1120@naver.com>
2022-06-19 23:18:03 +0900

    readInt

--- c/test.c
+++ c/test.c
@@ -64,106 +64,29 @@ int readInt(){
 
 void main()
 {
-    int sum = 11;
-    //1101
-    int b = 16;
-    printf("%d", 50 & 15);
-    // intTobit(16);
-/*
-a : 40  0010 1000
-a : 41  0010 1001
-a : 42  0010 1010
-a : 43  0010 1011
-a : 44  0010 1100
-a : 45  0010 1101
-a : 46  0010 1110
-a : 47  0010 1111
-a : 48  0011 0000
-a : 49  0011 0001
-a : 50  0011 0010
-a : 51  0011 0011
-a : 52  0011 0100
-a : 53  0011 0101
-a : 54  0011 0110
-a : 55  0011 0111
-a : 56  0011 1000
-a : 57  0011 1001
-a : 58  0011 1010
-a : 59  0011 1011
-a : 60  0011 1100
-a : 61  0011 1101
-a : 62  0011 1110
-a : 63  0011 1111
-a : 64  0100 0000
-a : 65  0100 0001
-a : 66  0100 0010
+    
+    // intTobit(20);
+    10 << 1;
 
-*/
+    //printf("\ntest : %d\n", 5 << 1);
 
-/*
-    int a = 40;
-    for (int i = 0; i < 27; i++)
+    for (int i = 0; i < 11; i++)
     {
-        printf("a : %d  ", a);
-        intTobit(a);
+        printf("i : %d ", i);intTobit(i);
         printf("\n");
-        a++;
     }
-*/
-// 48부터 57까지 0001 0000
 
-    // printf("%d",(1<<4) - (sum >> 0) );
-    //  int len = 7;
-    /*
-        for (int i = 0; i < 8; i++)
-        {
-            int n = (bit[i]-48) << len--;
-            //printf("%d\n", n);
-            sum += n;
-        }
-    */
+    char sum[6] = "91235";
 
-    //printf("%d", sum);
-    // printf("%d", print(b));
-    //  printf("%p %p", p, buff);
-    /*
-    if(p == buff+buff_size){
-        read(0, p=buff, buff_size);
-    }
-*/
-    // char c = ReadChar(); c & 16; ret = 10 * ret + (c & 15), c = ReadChar()
-    // unsigned int a = 1<<31;
-    // printf("%u", a);
-    
-    /*
-    for(int i=0; i<3; i++){
-        read(0, buff, buff_size);
-        //printf("%p %p %p\n", buff, p, buff+buff_size);
-        
-    }
-    */
-    //fread(buff, sizeof(char), buff_size, stdin);
-    
-    //printf("%s", buff);
-
-
-
-    //int i = 0;
-    
-    /*
-    int i = 0;
-    while (i<2)
+    int res = 0;
+    for (int i = 0; sum[i] != '\0'; ++i)
     {
-        if( (p+buff_size) ==buff ){
-            fread(buff, sizeof(char), p = buff, stdin);
-            i++;
-        }
-    }
-
-    int j = 0;
-    while (j < 20)
-    {
-        printf("%c", buff[j++]);
-    }
-    */
+        res = res *
+        printf("%d", sum[i] & 15);
+    }   
+    
+    
+    //printf("%d", 50 & 15);
+    
+    
 }

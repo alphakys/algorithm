2f5cd1ce447916258fc412dafb21fefe0ccaf842
alphakys <gys1120@naver.com>
2022-07-01 23:03:11 +0900

    22.7.1

--- c/t.c
+++ c/t.c
@@ -45,16 +45,16 @@ int compare(const void *a, const void *b){
     int num1 = *(int *)a;
     int num2 = *(int *)b;
 
-    if (num1 > num2)
+    if (num1 >= num2)
     {
         return 1;
     }
-    else if(num1 == num2)
+    /* else if(num1 == num2)
     {
        
-        a = NULL;
-        printf("p : %d\n", *(int *)a);
-    }
+        //a = NULL;
+        //printf("p : %d\n", *(int *)a);
+    } */
     else
     {
         return -1;

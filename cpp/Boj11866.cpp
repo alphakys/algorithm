f23ec206098a5c713b8037eb6284368eb58742c2
alphakys <gys1120@naver.com>
2022-07-23 13:17:33 +0900

    22.7.23

--- cpp/Boj11866.cpp
+++ cpp/Boj11866.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
 using namespace std;
 
+void print(const void *arr, int size, int _sizet){
+
+    if(_sizet>1)
+    {
+        arr = (int*)arr;
+    }
+    else
+    {
+        arr = (char*)arr;
+    }
+
+    if (size == 0)
+    {
+        //array
+        for (int i = 0; i < 7; i++)
+        {
+            //cout << arr[i] << " " ;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            //cout << arr[i];
+        }
+    }
+}
+
 int main(){
 
+    //풀이 2 큐
+    
+    int N, K;
+    cin >> N >> K;
+
+    int arr[N];
+
+    cout << sizeof(arr);
+    // print(arr, 10, sizeof(arr[0]));
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = i + 1;
+    }
+
+    /* 풀이 1 
     int N, K;
     cin >> N >> K;
 
@@ -36,7 +85,7 @@ int main(){
             
     }
 
-    cout << ">";
+    cout << ">"; */
 
     return 0;
 

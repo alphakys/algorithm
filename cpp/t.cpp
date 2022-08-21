33f40ec7f9eb112036af1a59673ba197c5d9d5cf
alphakys <gys1120@naver.com>
2022-08-21 23:17:04 +0900

    22.8.21 드디어 링크드리스트

--- cpp/t.cpp
+++ cpp/t.cpp
@@ -3,37 +3,74 @@
 using namespace std;
 
 typedef struct Node{
-    int value;
+
     Node* next;
+    int value;
 } Node;
 
 class Solution{
   
   public:
-    Node *head = nullptr;
+    Node* head = nullptr;
     //Function to insert a node at the beginning of the linked list.
-    Node* insertAtBegining(int x) {
+    void insertAtBegining(int x) {
         
-        if( head == nullptr ){ Node n; n.value = x; head = &n;}
-        else{ }
+        if( head == nullptr ){ head = new Node(); head->value = x; cout << "head : " << head << endl;}
+        else{ 
+            Node* curr = head;
+            cout << "첫 : " << curr << endl;
+            for(;curr->next!=nullptr; curr = curr->next)
+            { 
+                cout << "t: " << curr << endl; 
+            }
+            cout << curr <<endl;
+            curr->next = new Node();
+            curr->next->value = x;
+            cout << curr->next <<endl;
+
+            // nextNode = new Node();
+            // cout << nextNode << endl;
+            // nextNode->value = x;
+            // cout << "curr : " << curr->next << endl;
+            // cout << "hhh : " << head->next << endl;
+            // head = curr;
+            
+        }
     }
     
-    
     //Function to insert a node at the end of the linked list.
-    Node* insertAtEnd(Node *head, int x)  {
-       // Your code here
+    void insertAtEnd(Node *head, int x)  {
+        
+    }
+
+    void display()
+    {
+        Node* curr = head;
+        for(; curr!=nullptr; curr = curr->next)
+        {
+            cout << curr->value << " ";
+        }
+      
     }
 };
 
 int main(void){
+    
+    Solution *s = new Solution();
+    //cout <<1;
+    s->insertAtBegining(1);
+    s->insertAtBegining(2);
+    s->insertAtBegining(3);
+    
 
-    Solution s;
-    cout << &s.head << endl;
-    cout << &s << endl;
-    if(s.head){cout << "not null";}
-    else{
-        cout << "null";
-    }
+
+    cout <<"헤드1 : " << s->head->value << endl;
+
+    cout <<"헤드2 : " << s->head->next->value << endl;
+    cout <<"헤드3 : " << s->head->next->next->value << endl;
+    s->display();
+    
+    
     
     return 0;
 
---
+++ cpp/t1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+class Node {
+
+    public:
+        int data;
+        Node* next;// = nullptr;
+
+};
+
+typedef struct node {
+    Node* next;
+    int data;
+    
+
+}node;
+
+int main(){
+    
+    node a = NULL;
+    // Node* head = NULL;
+
+    // Node* curr;
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     if(head ==NULL){
+            
+    //         head = new Node();
+    //         head->data = i;
+    //         curr = head;
+    //     }else{
+    //         curr->next = new Node();
+    //         Node *n = curr->next;
+    //         n->data = i;
+    //         curr = n;
+    //     }
+    // }
+
+    // cout << head->data;
+    // cout << head->next->data;
+    // cout << head->next->next->data;
+
+    
+}

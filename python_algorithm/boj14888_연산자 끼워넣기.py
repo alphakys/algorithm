4d3c1311739fd223ac5ea57421007d9971a85c73
alphakys <gys1120@naver.com>
2023-07-17 18:41:41 +0900

    23.7.17

---
+++ python_algorithm/boj14888_연산자 끼워넣기.py
@@ -0,0 +1,7 @@
+import sys
+
+N = sys.stdin.readline()
+arr = [int(n) for n in sys.stdin.readline().split()]
+operator_arr = [int(n) for n in sys.stdin.readline().split()]
+
+
--- python_algorithm/boj2309_일곱난쟁이.py
+++ python_algorithm/boj2309_일곱난쟁이.py
@@ -19,14 +19,3 @@ for idx, i in enumerate(arr):
 
             sys.stdout.write('\n'.join([str(n) for n in sorted(arr)]))
             exit()
-
-
-# array([22, 41,  6, 16,  9,  1,  5]),
-# array([21,  9, 31, 11,  3,  4, 21]),
-# array([12, 13, 21,  1, 13, 31,  9]),
-# array([14, 27, 10,  9, 32,  2,  6]),
-# array([ 4, 27,  1, 10,  7,  2, 49]),
-# array([33,  2,  8,  3,  6,  5, 43]),
-# array([ 2,  1,  8, 35,  5, 11, 38]),
-# array([ 7,  6,  8, 10,  5, 60,  4]),
-# array([20,  2,  8,  1, 17,  4, 48])]

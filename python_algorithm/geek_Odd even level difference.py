5ff7d71e2a239ebbd863727416c64e14d0659c1d
alphakys <gys1120@naver.com>
2023-07-26 22:10:11 +0900

    23,7.26

---
+++ python_algorithm/geek_Odd even level difference.py
@@ -0,0 +1,135 @@
+# User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+
+class Solution:
+
+    def getLevelDiff(self, root):
+
+        if not root:
+            return []
+
+        odd_sum = root.data
+        even_sum = 0
+        i = 2
+        root_list = [root]
+        child_list = []
+
+        while root_list:
+
+            for node in root_list:
+                if node.left:
+                    child_list.append(node.left)
+                    num = node.left.data
+                    if i % 2 == 0:
+                        even_sum += num
+                    else:
+                        odd_sum += num
+
+                if node.right:
+                    child_list.append(node.right)
+                    num = node.right.data
+                    if i % 2 == 0:
+                        even_sum += num
+                    else:
+                        odd_sum += num
+            i += 1
+            root_list = child_list
+            child_list = []
+
+        return odd_sum - even_sum
+
+
+# Code here
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+import sys
+
+sys.setrecursionlimit(50000)
+# Contributed by Sudarshan Sharma
+from collections import deque
+
+
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        print(Solution().getLevelDiff(root))
+
+# } Driver Code Ends

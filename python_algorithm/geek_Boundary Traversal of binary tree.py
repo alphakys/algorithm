ca075dd0f34548f7a10b275a708cc121ded2850a
alphakys <gys1120@naver.com>
2023-08-09 18:21:42 +0900

    23.8.9

---
+++ python_algorithm/geek_Boundary Traversal of binary tree.py
@@ -0,0 +1,187 @@
+# User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+
+#         1
+#       /   \
+#      2     3
+#     / \   / \
+#    4   5 6   7
+#       / \
+#      8   9
+#
+
+# 1 2 4 8 9 6 7 3
+
+
+#             1
+#            /
+#           2
+#         /  \
+#        4    9
+#      /  \    \
+#     6    5    3
+#              /  \
+#             7     8
+#
+# 1 2 4 6 5 7 8
+
+
+#                 4
+#          10
+#      5         5
+#        6     7
+#      8     8     8
+#   11     3   4      1
+#  3     8 6     11 11
+# 5 8
+
+
+class Solution:
+
+    def order_util(self, root, arr):
+        if root.left:
+            self.order_util(root.left, arr)
+
+        if root.right:
+            self.order_util(root.right, arr)
+
+        if not root.left and not root.right:
+            arr.append(root.data)
+        return arr
+
+    def printBoundaryView(self, root):
+        origin_root = root
+        left_boundary_list = []
+        if root.left:
+
+            while root.left:
+                if root.left or root.right:
+                    left_boundary_list.append(root)
+                root = root.left
+
+            left_boundary_list = [n.data for n in left_boundary_list]
+            # print(left_boundary_list)
+            left_leaves = self.order_util(origin_root.left, [])
+            # print(left_leaves)
+            left_boundary_list.extend(left_leaves)
+
+        if origin_root.right:
+            root = origin_root.right
+            right_boundary_list = []
+            while root.right:
+                if root.left or root.right:
+                    right_boundary_list.append(root)
+                root = root.right
+
+            right_boundary_list = [n.data for n in right_boundary_list]
+            right_boundary_list.reverse()
+            right_leaves = self.order_util(origin_root.right, [])
+
+            left_boundary_list.extend(right_leaves)
+            left_boundary_list.extend(right_boundary_list)
+
+        return left_boundary_list
+
+
+# Code here
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# function should return a list containing the boundary view of the binary tree
+# {
+#  Driver Code Starts
+import sys
+
+import sys
+
+sys.setrecursionlimit(100000)
+# Contributed by Sudarshan Sharma
+from collections import deque
+
+
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    # Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        obj = Solution()
+        res = obj.printBoundaryView(root)
+        for i in res:
+            print(i, end=" ")
+        print('')
+# } Driver Code Ends

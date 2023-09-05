d560d61e95e31faca4bf3b5e05d43f3179a5b470
alphakys <gys1120@naver.com>
2023-09-05 19:18:44 +0900

    23.9.5

--- kubernetes_install.sh
+++ kubernetes_install.sh
@@ -23,3 +23,34 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://apt.kubernete
 echo $pw | sudo -S apt-get update
 echo $pw | sudo -S apt-get install -y kubelet kubeadm kubectl
 
+echo $pw | sudo -S kubeadm init --apiserver-advertise-address 192.168.56.30 --pod-network-cidr=20.96.0.0/12
+echo $pw | sudo -S kubeadm token create --print-join-command > ~/join.sh
+
+mkdir -p $HOME/.kube
+echo $pw | sudo -S cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+echo $pw | sudo -S chown $(id -u):$(id -g) $HOME/.kube/config
+
+# kubectl 자동완성 기능을 사용하기 위해선 bash-completion의 설치가 필요하다(이유는 의존성을 가지고 있기 때문에) 
+# 따라서 bash-completion을 먼저 설치한다.
+
+echo $pw | sudo -S apt-get install -y bash-completion
+# 위의 명령은 bash-completion의 기본 스크립트인 /usr/share/bash-completion/bash_completion 을 생성한다. 패키지 관리자에 따라, ~/.bashrc 파일에서 이 파일을 수동으로 소스(source)해야 한다.
+
+source /usr/share/bash-completion/bash_completion
+
+# 시스템 전체 사용자에게 적용하기 위해 bash_completion.d에 kubectl을 추가한다.
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
+# 두 방법 모두 동일하다. 셸을 다시 로드하면, kubectl 자동 완성 기능이 작동할 것이다. 셸의 현재 세션에서 bash 자동 완성을 활성화하려면 exec bash를 실행한다.
+
+exec bash
+
+# Calico 설치 / 정확하게 내용을 파악하지 못했다. 하지만 yaml 파일에 상세한 설정들이 기록되어 있는 것 같다.
+kubectl create -f https://raw.githubusercontent.com/kubetm/kubetm.github.io/master/yamls/k8s-install/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/kubetm/kubetm.github.io/master/yamls/k8s-install/calico-custom.yaml
+
+# 기존의 명령어를 통해 파일을 다운받았는데 내용은 redirect하는 파일이었다.
+# 최종 파일
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+

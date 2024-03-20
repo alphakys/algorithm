bedc1787303078c55e76d378e27d30ee3a905a30
alphakys <gys1120@gmail.com>
2024-03-20 13:25:56 +0900

    3.20

--- python_algorithm/boj11724.py
+++ python_algorithm/boj11724.py
@@ -1,44 +1,48 @@
 import sys
 from collections import deque
 
+
 N, M = map(lambda x: int(x), sys.stdin.readline().rstrip().split(' '))
 
 # if M == 0:
 #     print(1)
 #     exit()
 
-graph = [[0] * (N + 1) for i in range(N + 1)]
+adj_matrix = [[0] * (N + 1) for i in range(N + 1)]
 for _ in range(M):
     n, e = map(lambda x: int(x), sys.stdin.readline().rstrip().split(' '))
-    graph[n][e] = 1
-    graph[e][n] = 1
-
-for g in graph:
-    print(g)
+    adj_matrix[n][e] = 1
+    adj_matrix[e][n] = 1
 
 vis = [False] * (N + 1)
 
 stack = deque()
-cnt = 0
-for node in range(1, N + 1):
-    for edge in range(1, N + 1):
 
-        if graph[node][edge] == 1 and not vis[edge]:
-            cnt += 1
+connected_component = []
+for node in range(1, N + 1):
+    s = set()
+    for edge in range(1, N + 1):
+        if adj_matrix[node][edge] == 1 and not vis[edge]:
             vis[node] = True
             vis[edge] = True
             stack.append(edge)
+            s.add(node)
 
-            while len(stack):
+    while len(stack):
 
-                curr_node = stack.pop()
-                for e in range(1, N + 1):
-                    if graph[curr_node][e] == 1 and not vis[e]:
-                        stack.append(e)
-                        vis[e] = True
+        curr_node = stack.pop()
+        s.add(curr_node)
+        for e in range(1, N + 1):
+            if adj_matrix[curr_node][e] == 1 and not vis[e]:
+                stack.append(e)
+                vis[e] = True
 
-print(vis)
+    if s:
+        connected_component.append(s)
+
+answer = len(connected_component)
+
 if vis.count(False) > 1:
-    cnt += vis.count(False) - 1
+    answer += vis.count(False) - 1
 
-print(cnt)
+print(answer)

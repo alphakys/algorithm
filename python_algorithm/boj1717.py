d2908a2a08256b3220f7f20b1ef5be9c34c5c4bf
alphakys <gys1120@gmail.com>
2024-03-24 22:09:45 +0900

    3.24 union find

--- python_algorithm/boj1717.py
+++ python_algorithm/boj1717.py
@@ -1,23 +1,40 @@
 import sys
 
 
-def union(sa, sb, rep_l):
+def union(sa, sb, rep_l, rank):
+    if sa == sb:
+        return
+
     ## 핵심 개념은 tree의 height(rank)가 짧을수록 검색 속도는(find) 빨라진다.
     # 따라서 rank가 짧은 node에 연장하도록 한다.
     rep_a = find(sa, rep_l)
     rep_b = find(sb, rep_l)
+    if rep_a == rep_b:
+        return
 
-    rep_l[sb] = sa
+    if rank[rep_a] > rank[rep_b]:
+        rep_l[rep_b] = rep_a
+
+    elif rank[rep_a] == rank[rep_b]:
+        rep_l[rep_b] = rep_a
+        rank[rep_a] += 1
+    else:
+        rep_l[rep_a] = rep_b
+
+    # print(rep_l)
+    # print(rank)
 
 
 def find(node, par_tree):
-
     par_node = node
+    rank = 0
     while True:
         if par_tree[par_node] == par_node:
             break
 
         par_node = par_tree[par_node]
+        rank += 1
+
     return par_node
 
 
@@ -25,12 +42,13 @@ def main() -> None:
     N, M = map(lambda x: int(x), sys.stdin.readline().rstrip().split(' '))
 
     rep_l = [n for n in range(N + 1)]
+    rank = [0] * (N + 1)
 
     for _ in range(M):
         op, a, b = map(lambda x: int(x), sys.stdin.readline().rstrip().split(' '))
 
         if op == 0:
-            union(a, b, rep_l)
+            union(a, b, rep_l, rank)
         else:
             if op == 1:
                 pa = find(a, rep_l)
@@ -42,9 +60,8 @@ def main() -> None:
                     print("NO")
 
     # print(rep_l)
+    # print(rank)
 
-
-# UNION 1 3 UNION 3 5 UNION 31 5
 
 if __name__ == '__main__':
     main()

7fd90ec8765150b0844c36ed35b1a04371f9c833
alphakys <gys1120@gmail.com>
2024-04-30 21:54:51 +0900

    24.4.30

--- rust_algorithm/examples/leetcode1768.rs
+++ rust_algorithm/examples/leetcode1768.rs
@@ -5,27 +5,32 @@ fn main() {
 
     let mut answer = String::new();
 
-    let mut itr1 = word1.chars();
-    let mut itr2 = word2.chars();
-    let mut i = 0;
-    loop {
+    let mut zip = word1.chars().zip(word2.chars());
 
-        match (itr1.next(), itr2.next()) {
-            (Some(c1), Some(c2)) => {
-                answer.push(c1);
-                answer.push(c2);
-            },
-            (Some(c1), None) => {
-                answer.push(c1);
-            },
-            (None, Some(c2)) => {
-                answer.push(c2);
-            },
-            _ => {
-                break
-            }
-        }
+    for z in zip {
+        println!("{:?}", z);
     }
+    // let mut itr1 = word1.chars();
+    // let mut itr2 = word2.chars();
+    // let mut i = 0;
+    // loop {
+    //
+    //     match (itr1.next(), itr2.next()) {
+    //         (Some(c1), Some(c2)) => {
+    //             answer.push(c1);
+    //             answer.push(c2);
+    //         },
+    //         (Some(c1), None) => {
+    //             answer.push(c1);
+    //         },
+    //         (None, Some(c2)) => {
+    //             answer.push(c2);
+    //         },
+    //         _ => {
+    //             break
+    //         }
+    //     }
+    // }
 
     println!("{answer}", );
 

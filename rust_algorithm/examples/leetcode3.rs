0b87cd655ecbbf8a70c60817ba91c2283a97723b
alphakys <gys1120@gmail.com>
2024-05-05 17:41:00 +0900

    24.5.5

--- rust_algorithm/examples/leetcode3.rs
+++ rust_algorithm/examples/leetcode3.rs
@@ -1,22 +1,52 @@
 #![allow(unused)]
 
 
-fn main() {
+use std::cell::RefCell;
+use std::collections::HashSet;
 
-    let s: String = String::from("abcabcbb");
+fn main() {
+    let mut data = RefCell::new(None);
+    let inner = if data.borrow().is_none() {
+        let d = 1337;
+        *data.borrow_mut() = Some(d);
+        d
+    } else {
+        data.borrow().unwrap()
+    };
+
+    let s: String = String::from("abcbbcbb");
 
     let mut str_slice = s.as_bytes();
-    let mut hashmap = [-1; 128];
 
-    for (i, &c) in str_slice.iter().enumerate() {
-        if hashmap[c] > -1 {
+    let mut max_length = 0;
+    // left pointer
+    let mut left = 0;
+    // right pointer
+    let mut right = 0;
 
+    let mut set: HashSet<u8> = HashSet::new();
+    while right < str_slice.len() {
+        // get이 immutable borrow인 부분에 대해서는 너무 이해가 간다.
+        // get을 통해서 element를 return 받았는데 만약에 그 사이에 mut borrow를 통해서
+        // 값이 바뀌었다면 어떻게 할 것인가에 대해서 생각할 필요가 있다.
+        let result = set.get(&str_slice[right]);
+
+        if result.is_some(){
+            if max_length < set.len() { max_length = set.len(); }
+
+            while str_slice[left] != *result.unwrap() {
+                // remove all elements e for which f(&e) Imp` returns false
+                set = { set.remove(&str_slice[left]); set };
+                left += 1;
+            }
         }
-        else {
-            hashmap[c] = i;
+        else{
+            set.insert(str_slice[right]);
         }
 
+        right += 1;
     }
+
 
 
     // let mut answer = vec![];

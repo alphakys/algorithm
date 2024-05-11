cb3cdad49b80c1f27c180d5182b7921c4c615cb3
강용수 <74286771+alphakys@users.noreply.github.com>
2024-05-11 20:51:10 +0900

    2024.5.11

---
+++ rust_algorithm/examples/leetcode14.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let strs = ["flower","flow","flight"];
+    let max = strs[0].len();
+
+    strs[1..].iter().max();
+
+    let mut i = 0;
+
+    while i < {
+
+
+
+        i += 1;
+    }
+
+}

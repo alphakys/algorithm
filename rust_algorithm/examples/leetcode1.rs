46f2aff5e98932f7c058f8448e1ba5f25be4ed36
alphakys1 <gys1120@infludeo.com>
2024-05-12 18:03:11 +0900

    24.5.12

---
+++ rust_algorithm/examples/leetcode1.rs
@@ -0,0 +1,47 @@
+#![allow(unused)]
+
+use std::collections::{BTreeMap, HashMap};
+
+fn main() {
+    let nums = vec![2,7,11,15];
+    let target = 9;
+
+    let mut map = HashMap::new();
+    let mut answer = vec![];
+    for (i, n) in nums.iter().enumerate() {
+        let complement = target - n;
+        match map.get(&complement) {
+            Some(t) => {
+                answer.append(&mut vec![i, *t]);
+            }
+            None => { map.insert(n, i); }
+        }
+
+    }
+
+    println!("{:?}", answer);
+
+
+
+    // let mut tu: Vec::<(usize, i32)> = vec![];
+    // for (idx, &j) in nums.iter().enumerate() {
+    //     tu.push((idx, j));
+    // }
+    //
+    // tu.sort_by(|a, b| a.1.cmp(&b.1));
+    // let values = tu.iter().map(|t| t.1).collect::<Vec<_>>();
+    //
+    // let mut complement = 0;
+    // let mut answer = vec![];
+    // for (i, &n) in nums.iter().enumerate() {
+    //     complement = target - n;
+    //     match values.binary_search(&complement){
+    //         Ok(t) => {
+    //             if tu[t].0 != i { answer.push(i as i32); answer.push( tu[t].0 as i32); break }
+    //         },
+    //         Err(e) => ()
+    //     }
+    //
+    // }
+    // println!("{:?}", answer);
+}

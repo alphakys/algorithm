b07d4e9976a089d640cb5845a8e3bd3930064e98
alphakys <gys1120@gmail.com>
2024-07-04 00:08:08 +0900

    24.7.4

--- python_algorithm/leetcode66.py
+++ python_algorithm/leetcode66.py
@@ -1,19 +1,16 @@
 class Solution:
     def plusOne(self, digits):
-        _sum = 0
-        carry = 1
-        loop_index = len(digits) - 1
-        while loop_index >= 0 or carry:
-            _sum = digits[loop_index] + carry
-            if _sum > 9:
-                carry = 1
-                digits[loop_index] = 0
+        loop_idx = len(digits) - 1
+        while loop_idx >= 0:
+            # 계속해서 9 + 1 = 10 이 되지 않는다면 결국 while statement를 나오게 될 것이다.
+            if digits[loop_idx] < 9:
+                digits[loop_idx] += 1
                 return
-            else:
-                if loop_index == -1:
-                    digits.insert(0, _sum)
-                    break
-                digits[loop_index] = _sum
-                carry = 0
-            loop_index -= 1
-        return digits
+            digits[loop_idx] = 0
+            loop_idx -= 1
+        digits.insert(0, 1)
+
+if __name__ == '__main__':
+    digits = [9]
+    Solution().plusOne(digits)
+    print(digits)
---
+++ python_algorithm/leetcode83.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f"ListNode : {self.val}"
+
+
+
+def loop_linked_list(head):
+    while head:
+        print(head.val)
+        head = head.next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        root = head
+
+        while root:
+            next_node = root.next
+            while next_node and root.val == next_node.val:
+                next_node = next_node.next
+            root.next = next_node
+            root = root.next
+        loop_linked_list(head)
+
+
+if __name__ == '__main__':
+    inputs = [1,2,3,3,3,3,3]
+
+    root_node = ListNode(inputs[0])
+    head = root_node
+    for n in inputs[1:]:
+        while head.next:
+            head = head.next
+        head.next = ListNode(n)
+    Solution().deleteDuplicates(root_node)
